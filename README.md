8dfaf4f346531ca0d2e23dfb22e81cc577e68cbb
MJ Rossetti <datacreativellc@gmail.com>
2017-07-13 19:03:36 -0400

    Handle user inputs

--- README.md
+++ README.md
@@ -5,3 +5,16 @@
 Download the prepared CSV file from _______. Move it into the "data" directory. Its file path should be:
 
     data/products.csv
+
+## Installation
+
+```shell
+git clone __________
+cd ________
+```
+
+## Usage
+
+```shell
+python3 crud_app.py
+```
---
+++ crud_app.py
@@ -0,0 +1,24 @@
+menu = """
+-----------------------------------
+PRODUCTS INVENTORY APPLICATION
+-----------------------------------
+
+Welcome @s2t2!
+
+There are 20 products in the database.
+
+    operation | description
+    --------- | ------------------
+    'Create'  | Add a new product.
+    'Read'    | Show information about a product.
+    'Update'  | Edit an existing product.
+    'Destroy' | Delete an existing product.
+
+Please select an operation: """ # end of multi- line string
+
+crud_operation = input(menu)
+
+if crud_operation in ["Create", "Read", "Update", "Destroy"]:
+    print("YOU CHOSE", crud_operation.upper())
+else:
+    print("OOPS SORRY. PLEASE TRY AGAIN.")
