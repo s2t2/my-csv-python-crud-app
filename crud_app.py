b1201d45ed1c3b3e3c29689938aaaac9c3d30d0c
MJ Rossetti <datacreativellc@gmail.com>
2017-07-14 00:06:53 -0400

    List products

--- crud_app.py
+++ crud_app.py
@@ -20,6 +20,19 @@ with open(products_csv, "r") as csv_file:
 # HANDLE USER INPUT
 #
 
+def list_products():
+    print("LISTING PRODUCTS HERE")
+    for product in products:
+        print(" + Product #" + str(product["id"]) + ": " + product["name"])
+
+def show_product():
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id]
+    if product:
+        print("READING PRODUCT HERE", product)
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
+
 def create_product():
     print("OK. PLEASE PROVIDE THE PRODUCT'S INFORMATION...")
     product_id = len(products) # auto-increment product identifiers
@@ -29,14 +42,6 @@ def create_product():
         product[header] = input("The '{0}' is: ".format(header))
     products.append(product)
     print("CREATING PRODUCT HERE", product)
-
-def read_product():
-    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
-    product = [p for p in products if p["id"] == product_id]
-    if product:
-        print("READING PRODUCT HERE", product)
-    else:
-        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
 
 def update_product():
     print("UPDATING PRODUCT HERE")
@@ -55,8 +60,9 @@ There are {1} products in the database.
 
     operation | description
     --------- | ------------------
+    'List'    | Display a list of product identifiers and names.
+    'Show'    | Show information about a product.
     'Create'  | Add a new product.
-    'Read'    | Show information about a product.
     'Update'  | Edit an existing product.
     'Destroy' | Delete an existing product.
 
@@ -64,10 +70,12 @@ Please select an operation: """.format("@s2t2", len(products)) # end of multi- l
 
 crud_operation = input(menu)
 
-if crud_operation.title() == "Create":
+if crud_operation.title() == "List":
+    list_products()
+elif crud_operation.title() == "Show":
+    show_product()
+elif crud_operation.title() == "Create":
     create_product()
-elif crud_operation.title() == "Read":
-    read_product()
 elif crud_operation.title() == "Update":
     update_product()
 elif crud_operation.title() == "Destroy":
