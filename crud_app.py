2cb1745a17a39bf478623db9e7feecf34f4e06b4
MJ Rossetti <datacreativellc@gmail.com>
2017-07-13 22:08:05 -0400

    Prepare to perform operations

--- crud_app.py
+++ crud_app.py
@@ -1,30 +1,47 @@
 #from IPython import embed
 
-#menu = """
-#-----------------------------------
-#PRODUCTS INVENTORY APPLICATION
-#-----------------------------------
-#
-#Welcome @s2t2!
-#
-#There are 20 products in the database.
-#
-#    operation | description
-#    --------- | ------------------
-#    'Create'  | Add a new product.
-#    'Read'    | Show information about a product.
-#    'Update'  | Edit an existing product.
-#    'Destroy' | Delete an existing product.
-#
-#Please select an operation: """ # end of multi- line string
-#
-#crud_operation = input(menu)
-#
-#if crud_operation in ["Create", "Read", "Update", "Destroy"]:
-#    print("YOU CHOSE", crud_operation.upper())
-#else:
-#    print("OOPS SORRY. PLEASE TRY AGAIN.")
+menu = """
+-----------------------------------
+PRODUCTS INVENTORY APPLICATION
+-----------------------------------
 
+Welcome @s2t2!
+
+There are 20 products in the database.
+
+    operation | description
+    --------- | ------------------
+    'Create'  | Add a new product.
+    'Read'    | Show information about a product.
+    'Update'  | Edit an existing product.
+    'Destroy' | Delete an existing product.
+
+Please select an operation: """ # end of multi- line string
+
+crud_operation = input(menu)
+
+def create_product():
+    print("CREATING PRODUCT HERE")
+
+def read_product():
+    print("READING PRODUCT HERE")
+
+def update_product():
+    print("UPDATING PRODUCT HERE")
+
+def destroy_product():
+    print("DESTROYING PRODUCT HERE")
+
+if crud_operation.title() == "Create":
+    create_product()
+elif crud_operation.title() == "Read":
+    read_product()
+elif crud_operation.title() == "Update":
+    update_product()
+elif crud_operation.title() == "Destroy":
+    destroy_product()
+else:
+    print("OOPS SORRY. PLEASE TRY AGAIN.")
 
 #csv_path = "data/products.csv"
 #with open(csv_path,'r') as csv:
