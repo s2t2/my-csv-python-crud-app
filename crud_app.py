644bbe529097060ed4c913f65cc83bd11cf48c1b
MJ Rossetti <datacreativellc@gmail.com>
2017-07-13 20:57:06 -0400

    Read contents of CSV file

--- crud_app.py
+++ crud_app.py
@@ -1,24 +1,35 @@
-menu = """
------------------------------------
-PRODUCTS INVENTORY APPLICATION
------------------------------------
+#menu = """
+#-----------------------------------
+#PRODUCTS INVENTORY APPLICATION
+#-----------------------------------
+#
+#Welcome @s2t2!
+#
+#There are 20 products in the database.
+#
+#    operation | description
+#    --------- | ------------------
+#    'Create'  | Add a new product.
+#    'Read'    | Show information about a product.
+#    'Update'  | Edit an existing product.
+#    'Destroy' | Delete an existing product.
+#
+#Please select an operation: """ # end of multi- line string
+#
+#crud_operation = input(menu)
+#
+#if crud_operation in ["Create", "Read", "Update", "Destroy"]:
+#    print("YOU CHOSE", crud_operation.upper())
+#else:
+#    print("OOPS SORRY. PLEASE TRY AGAIN.")
 
-Welcome @s2t2!
 
-There are 20 products in the database.
 
-    operation | description
-    --------- | ------------------
-    'Create'  | Add a new product.
-    'Read'    | Show information about a product.
-    'Update'  | Edit an existing product.
-    'Destroy' | Delete an existing product.
 
-Please select an operation: """ # end of multi- line string
 
-crud_operation = input(menu)
+csv_path = "data/products.csv"
 
-if crud_operation in ["Create", "Read", "Update", "Destroy"]:
-    print("YOU CHOSE", crud_operation.upper())
-else:
-    print("OOPS SORRY. PLEASE TRY AGAIN.")
+with open(csv_path,'r') as csv:
+    output = csv.read()
+    print(type(output))
+    print(output)
